9772a6d8e9b40a2fb9e74f5c79a730e8cea453b4
msullivan <mykalsullivan@duck.com>
2024-09-10 14:26:11 -0500

    Added methods to cpoint.h based on those from Qt.

--- CursesGUI/src/cpoint.h
+++ CursesGUI/src/cpoint.h
@@ -5,5 +5,28 @@
 #pragma once
 
 class CPoint {
+public:
+    CPoint() = default;
+    CPoint(int xpos, int ypos);
 
+public:
+    bool isNull() const;
+    int manhattanLength() const;
+    int &rx();
+    int &ry();
+    void setX(int x);
+    void setY(int y);
+    CPoint transposed() const;
+    int x() const;
+    int y() const;
+    CPoint &operator*=(float factor);
+    CPoint &operator*=(double factor);
+    CPoint &operator*=(int factor);
+    CPoint &operator+=(const CPoint &point);
+    CPoint &operator-=(const CPoint &point);
+    CPoint &operator/=(float divisor);
+    CPoint &operator/=(double divisor);
+    CPoint &operator/=(int divisor);
+
+    static int dotProduct(const CPoint &p1, const CPoint &p2);
 };
